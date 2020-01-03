5391bfb5a3ca6fae9a4f7a49dfa4627194d9862c
Jincheng-Sun <sjc951213@gmail.com>
2020-01-03 18:18:30 -0500

    add model components

---
+++ framework/model.py
@@ -0,0 +1,117 @@
+from abc import abstractmethod
+from utils import logger
+import torch
+import torch.nn as nn
+from torch.utils.tensorboard import SummaryWriter
+
+class Model():
+    def __init__(self, save_path, log_path):
+        self.save_path = save_path
+        self.log_path = log_path
+        self.model = None
+        self.classifier = None
+        self.parameters = None
+        self.optimizer = None
+        self.train_logger = None
+        self.eval_logger = None
+        self.summary_writer = None
+
+    @abstractmethod
+    def loss(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def train(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def evaluate(self, **kwargs):
+        pass
+
+    @abstractmethod
+    def checkpoint(self, **kwargs):
+        pass
+
+    def data_parallel(self):
+        # If GPU available, move the graph to GPU(s)
+        self.CUDA_AVAILABLE = self.check_cuda()
+        if self.CUDA_AVAILABLE:
+            device_ids = list(range(torch.cuda.device_count()))
+            self.model = nn.DataParallel(self.model, device_ids)
+            self.classifier = nn.DataParallel(self.classifier, device_ids)
+            self.model.to('cuda')
+            self.classifier.to('cuda')
+            assert (next(self.model.parameters()).is_cuda)
+            assert (next(self.classifier.parameters()).is_cuda)
+            pass
+
+        else:
+            print('CUDA not found or not enabled, use CPU instead')
+
+    def set_optimizer(self, Optimizer, lr, **kwargs):
+        self.optimizer = Optimizer(self.parameters, lr=lr, **kwargs)
+
+    def set_logger(self, mode='a'):
+        self.train_logger = logger('train',self.log_path + '-train', mode=mode)
+        self.eval_logger = logger('eval', self.log_path + '-eval', mode=mode)
+
+    def set_summary_writer(self):
+        self.summary_writer = SummaryWriter(self.log_path + 'tensorboard')
+
+    def check_cuda(self):
+        if torch.cuda.is_available():
+            print("INFO: CUDA device exists")
+            return torch.cuda.is_available()
+
+    def count_parameters(self):
+        try:
+            assert self.model != None
+            model_params = sum(p.numel() for p in self.model.parameters() if p.requires_grad)
+            print('Number of Model Parameters: %d'%model_params)
+            for name, param in self.model.named_parameters():
+                if param.requires_grad:
+                    print(name, param.numel())
+        except:
+            print('No Model specified')
+
+        try:
+            assert self.classifier != None
+            classifier = sum(p.numel() for p in self.classifier.parameters() if p.requires_grad)
+            print('Number of Model Classifier: %d' % classifier)
+            for name, param in self.classifier.named_parameters():
+                if param.requires_grad:
+                    print(name, param.numel())
+        except:
+            print('No Classifier specified')
+
+
+    def resume_checkpoint(self, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+
+        if self.model != None:
+            model_state_dict = checkpoint['model_state_dict']
+            self.model.load_state_dict(model_state_dict)
+            self.model.train()
+
+        classifier_state_dict = checkpoint['classifier_state_dict']
+        self.classifier.load_state_dict(classifier_state_dict)
+        self.classifier.train()
+
+    def save_model(self, checkpoint, save_path):
+        torch.save(checkpoint, save_path)
+
+    def load_model(self, checkpoint_path):
+        checkpoint = torch.load(checkpoint_path)
+
+        if self.model != None:
+            model_state_dict = checkpoint['model_state_dict']
+            self.model.load_state_dict(model_state_dict)
+            self.model.eval()
+
+        classifier_state_dict = checkpoint['classifier_state_dict']
+        self.classifier.load_state_dict(classifier_state_dict)
+        self.classifier.eval()
+
+
+
+
---
+++ utils.py
@@ -0,0 +1,101 @@
+import logging
+class logger():
+    def __init__(self, logger_name, log_path, mode, level=logging.INFO, format = "%(asctime)s - %(message)s"):
+        self.logging = logging.getLogger(logger_name)
+        self.logging.setLevel(level)
+        fh = logging.FileHandler(log_path, mode)
+        fh.setLevel(level)
+        sh = logging.StreamHandler()
+        sh.setLevel(logging.INFO)
+        formatter = logging.Formatter(format)
+        fh.setFormatter(formatter)
+        sh.setFormatter(formatter)
+        self.logging.addHandler(fh)
+        self.logging.addHandler(sh)
+
+    def info(self, msg):
+        return self.logging.info(msg)
+
+class TrainingControl():
+    def __init__(self, max_step, evaluate_every_nstep, print_every_nstep):
+        self.state_dict = {
+            'epoch': 0,
+            'batch': 0,
+            'step': 0,
+            'step_to_evaluate': False,
+            'step_to_print': False,
+            'step_to_stop': False
+        }
+        self.max_step = max_step
+        self.eval_every_n = evaluate_every_nstep
+        self.print_every_n = print_every_nstep
+        self.current_epoch = 0
+        self.current_batch = 0
+        self.current_step = 0
+
+    def __call__(self, batch):
+        self.current_step += 1
+        self.state_dict['batch'] = batch
+        self.state_dict['step'] = self.current_step
+        self.state_dict['step_to_evaluate'] = np.equal(np.mod(self.current_step, self.eval_every_n), 0)
+        self.state_dict['step_to_print'] = np.equal(np.mod(self.current_step, self.print_every_n), 0)
+        self.state_dict['step_to_stop'] = np.equal(self.current_step, self.max_step)
+        return self.state_dict
+
+    def set_epoch(self, epoch):
+        self.state_dict['epoch'] = epoch
+
+    def reset_state(self):
+        self.state_dict = {
+            'epoch': 0,
+            'batch': 0,
+            'step': 0,
+            'step_to_evaluate': False,
+            'step_to_print': False,
+            'step_to_stop': False
+        }
+        self.current_epoch = 0
+        self.current_batch = 0
+        self.current_step = 0
+
+
+class EarlyStopping():
+    def __init__(self, patience, mode='best'):
+        self.patience = patience
+        self.mode = mode
+        self.best_loss = 9999
+        self.waitting = 0
+        self.state_dict = {
+            'save': False,
+            'break': False
+        }
+
+    def __call__(self, val_loss):
+        self.state_dict['save'] = False
+        self.state_dict['break'] = False
+
+        if val_loss <= self.best_loss:
+            self.best_loss = val_loss
+            self.waitting = 0
+            self.state_dict['save'] = True
+
+        else:
+            self.waitting += 1
+
+            if self.mode == 'best':
+                self.state_dict['save'] = False
+            else:
+                self.state_dict['save'] = True
+
+            if self.waitting == self.patience:
+                self.state_dict['break'] = True
+
+        return self.state_dict
+    def reset_state(self):
+        self.best_loss = 9999
+        self.waitting = 0
+        self.state_dict = {
+            'save': False,
+            'break': False
+        }
+
