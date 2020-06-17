0584784cc553cb43c81aff98749460934060da84
Jincheng-Sun <sjc951213@gmail.com>
2020-06-16 23:54:05 -0400

    update module version 2

--- SAFS/Layers.py
+++ SAFS/Layers.py
@@ -16,21 +16,21 @@ class ScaledDotProduction(nn.Module):
     def forward(self, query, key, value):
         '''
             Arguments:
-                query {Tensor, shape: [batch, d_k, d_out]} -- query
-                key {Tensor, shape: [batch, d_k, n_candidate]} -- key
-                value {Tensor, shape: [batch, d_v, n_candidate]} -- value
+                query {Tensor, shape: [batch, n_subsets, d_k]} -- query
+                key {Tensor, shape: [batch, d_k, n_subsets]} -- key
+                value {Tensor, shape: [batch, n_subsets, sub_size]} -- value
 
             Returns:
                 output {Tensor, shape [n_head * batch, n_depth, n_vchannel * d_features] -- output
                 attn {Tensor, shape [n_head * batch, n_depth, n_depth] -- reaction attention
         '''
-        attn = torch.bmm(query.transpose(2, 1), key)  # [batch, d_out, n_candidate]
+        attn = torch.bmm(query, key)  # [batch, d_out, n_candidate]
         # How should we set the temperature
         attn = attn / self.temperature
 
         attn = self.softmax(attn)  # softmax over d_f1
         attn = self.dropout(attn)
-        output = torch.bmm(attn, value.transpose(2, 1))  # [batch, d_out, d_v]
+        output = torch.bmm(attn, value)  # [batch, d_out, d_v]
 
         return output, attn
 
@@ -61,7 +61,7 @@ class SelfAttentionLayer(nn.Module):
         self.d_k = d_k
         self.d_v = d_v
         self.d_out = d_out
-        self.stride_self = np.ceil(np.divide(d_features, d_out)).astype(int)
+        self.stride_self = torch.ceil(np.divide(d_features, d_out)).astype(int)
         self.n_replica = n_replica
         self.shuffled_index = f_shuffle(d_features, n_replica)
 
@@ -80,16 +80,15 @@ class SelfAttentionLayer(nn.Module):
         self.n_candidate = np.ceil(np.divide(d_features, stride)).astype(int)
 
         self.same_pad = (self.n_candidate - 1) * stride + kernel
-        self.padding2d = nn.ConstantPad2d((0, self.same_pad-d_features, 0, 0), 0)
+        self.padding2d = nn.ConstantPad2d((0, self.same_pad - d_features, 0, 0), 0)
 
-        self.attention = ScaledDotProduction(temperature=1)
+        self.attention = ScaledDotProduction(temperature=torch.sqrt(self.stride_self))
 
         self.layer_norm = nn.LayerNorm(d_features)
         self.dropout = nn.Dropout(dropout)
 
         ### Use Bottleneck? ###
         self.bottleneck = Bottleneck(d_out, d_out)
-
 
     def forward(self, features):
         '''
@@ -103,7 +102,8 @@ class SelfAttentionLayer(nn.Module):
         d_features, d_out, n_replica, shuffled_index \
             = self.d_features, self.d_out, self.n_replica, self.shuffled_index
 
-        residual = features
+        if d_features == d_out:
+            residual = features
 
         query = self.layer_norm(features)
 
@@ -119,13 +119,16 @@ class SelfAttentionLayer(nn.Module):
 
         query = self.query(query.unsqueeze(1))  # shape: [batch, d_k, d_out]
         key = self.key(
-            shuffled_features.unsqueeze(1)).view(-1, n_replica, self.d_k, self.n_candidate)  # shape: [batch, d_k, n_candidate], n_candidate = (n_replica * d_features) / stride
-        value = self.value(shuffled_features.unsqueeze(1)).view(-1, n_replica, self.d_v, self.n_candidate)  # shape: [batch, d_v, n_candidate]
+            shuffled_features.unsqueeze(1)).view(-1, n_replica, self.d_k,
+                                                 self.n_candidate)  # shape: [batch, d_k, n_candidate], n_candidate = (n_replica * d_features) / stride
+        value = self.value(shuffled_features.unsqueeze(1)).view(-1, n_replica, self.d_v,
+                                                                self.n_candidate)  # shape: [batch, d_v, n_candidate]
 
         key = key.transpose(2, 1).contiguous().view(-1, self.d_k, n_replica * self.n_candidate)
         value = value.transpose(2, 1).contiguous().view(-1, self.d_v, n_replica * self.n_candidate)
 
-        output, attn = self.attention(query, key, value)  # shape: [batch, d_out, d_v], [batch, d_out, n_candidate]
+        output, attn = self.attention(query.transpose(2, 1), key,
+                                      value.transpose(2, 1))  # shape: [batch, d_out, d_v], [batch, d_out, n_candidate]
         output = output.transpose(2, 1).contiguous()  # shape: [batch, d_v, d_out]
         output = self.conv(output).view(-1, d_out)  # shape: [batch, d_out]
         output = self.dropout(output)
@@ -135,5 +138,101 @@ class SelfAttentionLayer(nn.Module):
 
         ### Use Bottleneck? ###
         output = self.bottleneck(output)
+
+        return output, attn
+
+
+class SelfAttentionLayer_V2(nn.Module):
+    def __init__(self, f_shuffle, m, d, n, kernel, d_k, d_v, h, dropout=0.1):
+        super().__init__()
+        self.d_features = m
+        self.d_out = d
+        self.n_subsets = n
+        self.d_k = d_k
+        self.d_v = d_v
+        self.n_heads = h
+        self.shuffled_index = f_shuffle(m, h)
+
+        k = np.ceil(np.divide(m, n)).astype(int)  # Subset size
+        self.sub_size = k
+        m_new = k * n
+
+        self.query = nn.Linear(k, h * d_k, bias=False)
+        self.key = nn.Conv1d(h, h * d_k, groups=h, kernel_size=k, stride=k,
+                             bias=False)  # Act as a linear layer for multi heads
+        self.value_1 = nn.Conv1d(h, h * d_v, groups=h, kernel_size=kernel, bias=False)
+        self.value_2 = nn.Conv1d(h * d_v, h, groups=h, kernel_size=1, bias=False)
+        self.d_out_per_subset = np.divide(d, n).astype(int)
+        self.fc = nn.Linear(h * k, self.d_out_per_subset, bias=False)
+
+        nn.init.xavier_normal(self.query.weight)
+        nn.init.xavier_normal(self.key.weight)
+        nn.init.xavier_normal(self.value_1.weight)
+        nn.init.xavier_normal(self.value_2.weight)
+        nn.init.xavier_normal(self.fc.weight)
+
+        self.q_pad = nn.ConstantPad1d((0, m_new - m), 0)
+        self.k_pad = nn.ConstantPad2d((0, m_new - m, 0, 0), 0)
+        self.v_pad = nn.ConstantPad2d((0, kernel - 1, 0, 0), 0)
+
+        self.attention = ScaledDotProduction(temperature=np.sqrt(m))
+
+        self.layer_norm = nn.LayerNorm(m)
+        self.dropout = nn.Dropout(dropout)
+
+        ## Use Bottleneck? ###
+        self.bottleneck = Bottleneck(k * h, k)
+
+    def forward(self, features):
+        '''
+            Arguments:
+                feature_1 {Tensor, shape [batch, d_features]} -- features
+
+            Returns:
+                output {Tensor, shape [batch, d_features]} -- output
+                attn {Tensor, shape [n_head * batch, d_features, d_features]} -- self attention
+        '''
+        d_features, n_subsets, sub_size, n_heads, shuffled_index \
+            = self.d_features, self.n_subsets, self.sub_size, self.n_heads, self.shuffled_index
+
+        # if d_features == d_out:
+        #     residual = features
+
+        query = self.layer_norm(features)
+
+        # d_features_ceil = d_out * stride
+        #
+        # features = F.pad(features, (0, d_features_ceil - d_features), value=0)  # shape: [batch, d_features_ceil]
+
+        shuffled_features = features[:, shuffled_index]  # shape: [batch, n_heads * d_features]
+        shuffled_features = shuffled_features.view(-1, n_heads, d_features)
+        shuffled_features = self.k_pad(shuffled_features)
+
+        query = self.q_pad(query)
+        query = self.query(query.view(-1, n_subsets, sub_size))  # shape: [batch, d_k, d_out]
+        query = query.view(-1, n_subsets, n_heads, self.d_k).transpose(1, 2).contiguous().view(-1, n_subsets, self.d_k)
+
+        key = self.key(
+            shuffled_features)  # shape: [batch, d_k, n_candidate], n_candidate = (n_heads * d_features) / stride
+        key = key.view(-1, self.d_k, n_subsets)  # shape: [batch, d_k, d_out]
+
+        value = shuffled_features.view(-1, n_heads, n_subsets, sub_size).transpose(1, 2).contiguous().view(-1, n_heads,
+                                                                                                           sub_size)
+        value_residual = value
+        value = self.v_pad(value)
+        value = self.value_1(value)  # shape: [batch, d_v, n_candidate]
+        value = self.value_2(F.relu(value))
+        value = value + value_residual
+        value = value.view(-1, n_subsets, n_heads, sub_size).transpose(1, 2).contiguous().view(-1, n_subsets, sub_size)
+
+        output, attn = self.attention(query, key, value)  # shape: [batch, d_out, d_v], [batch, d_out, n_candidate]
+        output = output.view(-1, n_heads, n_subsets, sub_size).transpose(1, 2).contiguous().view(-1, n_subsets,
+                                                                                                 n_heads * sub_size)  # shape: [batch, d_v, d_out]
+        output = self.fc(output)  # shape: [batch, d_out]
+        output = self.dropout(output).view(-1, self.n_subsets * self.d_out_per_subset)
+
+
+        # ### Use Bottleneck? ###
+        # output = self.bottleneck(output)
 
         return output, attn
--- SAFS/Modules.py
+++ SAFS/Modules.py
@@ -1,5 +1,6 @@
 import torch.nn as nn
-from SAFS.Layers import SelfAttentionLayer
+from SAFS.Layers import SelfAttentionLayer, SelfAttentionLayer_V2
+
 
 class SelfAttentionFeatureSelection(nn.Module):
     def __init__(self, f_shuffle, d_features, d_out_list, kernel, stride, d_k, d_v, n_replica):
@@ -8,7 +9,8 @@ class SelfAttentionFeatureSelection(nn.Module):
         d_out_list.insert(0, d_features)
 
         self.layers = nn.ModuleList([
-            SelfAttentionLayer(f_shuffle, d_in, d_out, kernel=kernel, stride=stride, d_k=d_k, d_v=d_v, n_replica=n_replica,
+            SelfAttentionLayer(f_shuffle, d_in, d_out, kernel=kernel, stride=stride, d_k=d_k, d_v=d_v,
+                               n_replica=n_replica,
                                ) for d_in, d_out in zip(d_out_list, d_out_list[1:])])
 
         ## TODO: Is layer_norm at the end really necessary?
@@ -26,6 +28,7 @@ class SelfAttentionFeatureSelection(nn.Module):
 
         return features, self_attn_list
 
+
 class LinearClassifier(nn.Module):
     def __init__(self, d_in, d_hid, d_out):
         super(LinearClassifier, self).__init__()
@@ -42,3 +45,29 @@ class LinearClassifier(nn.Module):
         x = self.activation(x)
         x = self.fc2(x)
         return x
+
+
+class SelfAttentionFeatureSelection_V2(nn.Module):
+    def __init__(self, f_shuffle, d_features, n_subset_list, d_out_list, kernel, stride, d_k, d_v, h):
+        super().__init__()
+
+        d_out_list.insert(0, d_features)
+
+        self.layers = nn.ModuleList([
+            SelfAttentionLayer_V2(f_shuffle, d_in, d_out, n_sub, kernel, d_k, d_v, h,
+                                  ) for n_sub, d_in, d_out in zip(n_subset_list, d_out_list, d_out_list[1:])])
+
+        ## TODO: Is layer_norm at the end really necessary?
+        self.layer_norm = nn.LayerNorm(d_out_list[-1], eps=1e-6)
+
+    def forward(self, features):
+        self_attn_list = []
+
+        for layer in self.layers:
+            features, self_attn = layer(
+                features)
+            self_attn_list += [self_attn]
+
+        features = self.layer_norm(features)
+
+        return features, self_attn_list
